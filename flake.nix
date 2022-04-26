b1fd2f6685fff4bb643670bcb9854cb1977a22be
khaled agrama <khaled@users.noreply.github.com>
2022-04-25 20:11:02 -0400

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -39,6 +39,43 @@
           doCheck = true;
 
         };
+        
+        packages.x86_64-linux.hello2-garnix =
+      let pkgs = import nixpkgs {
+        system = "x86_64-linux";
+      };
+      in pkgs.stdenv.mkDerivation {
+          name = "hello2-garnix";
+          src = ./.;
+
+          unpackPhase = ":";
+
+          buildPhase = ''
+            echo "Just building some things, don't mind me"
+            cat > an-executable <<EOF
+            echo "Hello from an executable!"
+            EOF
+            chmod +x an-executable
+          '';
+
+          checkPhase = ''
+            echo "Looking around to see if anything is amiss.."
+            OUTPUT=$(./an-executable)
+            if [ "$OUTPUT" != "Hello from an executable!" ]; then
+              echo "Test failed!"
+              exit 1
+            fi
+          '';
+
+          installPhase = ''
+            mkdir -p $out/bin
+            cp an-executable $out/bin/
+          '';
+
+
+          doCheck = true;
+
+        };
 
 
     defaultPackage.x86_64-linux = self.packages.x86_64-linux.hello-garnix;
